d2b859493f65b991706640dba1b932063b50bbbd
Joshua Ainsley <jainsley@gmail.com>
2014-04-16 17:06:29 -0400

    updated ensembl mining

--- get_ensembl_attributes.R
+++ get_ensembl_attributes.R
@@ -1,4 +1,6 @@
-setwd("C:/Users/jainsl01/Copy/R_Code/Projects/DendLocML")
+#setwd("C:/Users/jainsl01/Copy/R_Code/Projects/DendLocML")
+setwd("~/Copy/R_Code/Projects/DendLocML")
+
 
 library("biomaRt")
 
@@ -24,7 +26,20 @@ genes <- c('ENSMUSG00000024617','ENSMUSG00000033981')
 # Putting the commands in a for loop so that they were queried one at a time works.
 ###################################################################
 
+
+#Functions for screening ensembl attributes
+#Function to get mart data for all integers specified simultaneously
 get_mart_data <- function(range=0) {
+  test_attribs <- attributes[range,1]
+  attribs <- c('ensembl_gene_id', test_attribs)
+  temp <- getBM(attributes=attribs, filters = 'ensembl_gene_id', values = genes, mart = ensembl)
+  print(temp)
+}
+
+#get_mart_data(982)
+
+#Function to get mart data for all integers specified one at a time in a loop
+get_mart_data_loop <- function(range=0) {
   for (i in range) {
     test_attribs <- attributes[i,1]
     attribs <- c('ensembl_gene_id', test_attribs)
@@ -34,4 +49,39 @@ get_mart_data <- function(range=0) {
   }
 }
 
-get_mart_data(1:10)
+get_mart_data_loop(1:6)
+
+#Read in screened ensembl attributes
+#library(xlsx)
+#all.attributes <- read.xlsx("ensemblAttributes.xlsx", 1)
+all.attributes <- read.csv("ensemblAttributes.csv")
+screened.attributes <- all.attributes[all.attributes$ML == 'y',]
+attrToGet <- as.character(screened.attributes[,1])
+
+#Function to collect the number of output lines for each screened ensembl attribute.
+#This is necessary to determine how best to collect the data.
+write_mart_data_loop <- function(range=0) {
+  output <- data.frame()
+  for (i in range) {
+    test_attribs <- attrToGet[i]
+    attribs <- c('ensembl_gene_id', test_attribs)
+    temp <- getBM(attributes=attribs, filters = 'ensembl_gene_id', values = genes, mart = ensembl)
+    output[i,1] <- test_attribs
+    output[i,2] <- nrow(temp)
+    Sys.sleep(1)
+  }
+  return(output)
+}
+
+output_rows <- write_mart_data_loop(1:length(attrToGet))
+#write.csv(output_rows, "output_rows.csv")
+
+#Function to get biomart data from character vector input
+get_mart_data_char <- function(char.ensembl, char.attribs) {
+  attribs <- c('ensembl_gene_id', char.attribs)
+  output <- getBM(attributes=attribs, filters = 'ensembl_gene_id', values = char.ensembl, mart = ensembl)
+  return(output)
+}
+
+input.attributes <- c("ensembl_transcript_id", "start_position", "end_position")
+test <- get_mart_data_char(genes, input.attributes)
