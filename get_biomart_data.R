c5712976b6c3d7c79832dcc1f77f7df306302775
Joshua Ainsley <jainsley@gmail.com>
2014-04-26 17:09:34 -0400

    Updates to biomart data

--- get_biomart_data.R
+++ get_biomart_data.R
@@ -7,18 +7,18 @@ library("biomaRt")
 rm(list = ls())
 
 #Set up mart
-#ensembl <- useMart("ensembl",dataset="mmusculus_gene_ensembl")
+ensembl <- useMart("ensembl",dataset="mmusculus_gene_ensembl")
 #ensembl_genes <- getBM(attributes = "ensembl_gene_id", mart = ensembl)
 #write.csv(ensembl_genes, "./data/ensembl_genes.txt", quote=FALSE, row.names=FALSE, col.names=FALSE)
 
 #Read in ensembl gene names
-ensembl_genes <- read.table("./data/ensembl_genes.txt", header=TRUE)
+ensembl_genes <- read.table("./data/ensembl_genes.txt", header=TRUE, stringsAsFactors = FALSE)
 
 #Read in attributes to use
-attributes <- read.csv('./data/output_rows.csv', stringsAsFactors = FALSE)
+attributes <- read.csv('./data/output_rows', stringsAsFactors = FALSE)
 
 #Get simple attributes
-#simple.attr <- subset(attributes$Attribute, attributes$RowNum == 2)
+simple.attr <- subset(attributes$Attribute, attributes$RowNum == 2)
 
 #Function to get biomart data from character vector input
 get_mart_data_loop <- function(char.ensembl, char.attribs) {
@@ -27,7 +27,7 @@ get_mart_data_loop <- function(char.ensembl, char.attribs) {
     temp_output <- getBM(attributes=attribs, filters = 'ensembl_gene_id', values = char.ensembl, mart = ensembl)
     write.csv(temp_output, paste0("./data/",i,".csv"), quote=FALSE)
     Sys.sleep(1)
-  }  
+  }
 }
 
-get_mart_data_loop(ensembl_genes, attributes$Attribute)
+get_mart_data_loop(ensembl_genes[,1], attributes$Attribute)
